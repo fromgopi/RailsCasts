e09429725609d6194cae20c552cbcdd31a92ce7f
Gopi Krishna M <fromgopi@yahoo.com>
2018-10-08 21:26:55 +0530

    Update map_controller.rb
    Removed some Info.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
 
   def index
 
-    @api_key = 'AIzaSyBd-zAiP5BziIKMOGKvZJ8CoP1YtnxR4Ww'
+    @api_key = '<Your Google Map API key.>'
     @map_center = [17.410875, 78.482226]
     @map_zoom = 13
 
